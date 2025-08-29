5b3e871eb0eb338c7ef4c4eeed1399b35162408c
HashingTuna <38752407+Hrztrm@users.noreply.github.com>
2025-08-29 14:59:18 +0800

    Update and rename Install-Sysmon.ps1 to Install-Sysmon-w-Config.ps1

--- Install-Sysmon-w-Config.ps1
+++ Install-Sysmon-w-Config.ps1
@@ -6,3 +6,6 @@ Expand-Archive -Path $env:Temp\sysmon.zip -DestinationPath $env:Temp
 iwr https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig-with-filedelete.xml -outfile $env:Temp\sysmonconfig.xml
 
 & $env:Temp\Sysmon.exe -i $env:Temp\sysmonconfig.xml -accepteula
+
+Remove-Item -Path "$env:Temp\Sysmon.*"
+Remove-Item -Path "$env:Temp\sysmonconfig.xml"
